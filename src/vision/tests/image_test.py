0a3c691a567e56e1e9b25c4d3335563dbe0e230e
admin <186482@upslp.edu.mx>
2026-07-16 02:05:27 -0600

    feat(navigation): add state machine for lap counting and direction logic

--- src/vision/tests/image_test.py
+++ src/vision/tests/image_test.py
@@ -6,7 +6,7 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from processing.image_manager import ImageManager
 
 image = ImageManager()
-image.run_test()
+#image.run_test()
 
 #To test using an image, uncomment the line below and change the last parameter to the file's path
-#image.run_test_from_image(os.path.join(os.path.dirname(__file__), "file_path")) 
+image.run_test_from_image(os.path.join(os.path.dirname(__file__), "vision3.png")) 
