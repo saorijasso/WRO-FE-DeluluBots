2f987e163fbc53ac42a71cba41684d52a2faeb3f
mario-xyza <jesusmmoralesguzm@gmail.com>
2026-09-18 03:42:42 -0600

    test

--- src/vision/tests/open_test.py
+++ src/vision/tests/open_test.py
@@ -9,6 +9,6 @@ if str(SRC_DIR) not in sys.path:
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))    
 
 from processing.image_manager import ImageManager
-
+#
 image = ImageManager()
 image.run_open_test()
